d2f2b4688a2241f895195c313013d2d5216cec70
MrAbas <abasov2016@list.ru>
2023-10-08 16:36:48 +0300

    Конечный вариант

--- js/script.js
+++ js/script.js
@@ -4,3 +4,7 @@ console.log(title);
 function showAlert(event) {
   alert("Всё работает");
 }
+
+let elem = document.querySelector("#anchor");
+elem.classList.toggle("styles_button");
+elem.classList.toggle("styles_button2");
